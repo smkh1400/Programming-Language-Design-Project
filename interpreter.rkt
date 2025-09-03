0be4c39ab2f2a55161af114ef500978c42231d46
smkh1400 <mz2012kh82@gmail.com>
2025-09-04 00:18:23 +0330

    phase 2 q3

--- interpreter.rkt
+++ interpreter.rkt
@@ -160,7 +160,7 @@
     )
 )
 
-(define input-program (open-input-file "q2.prog"))
+(define input-program (open-input-file "q3.prog"))
 (define tokens (lex-all input-program))
 (define token-generator (make-token-generator tokens))
 (define parse-result (parse-full token-generator))
